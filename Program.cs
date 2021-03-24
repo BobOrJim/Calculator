f497496eeb84ff695621acd483438d44f9df5326
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2021-03-24 10:54:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignment1 App = new Assignment1();
+            App.Run(); //This is a Task. If we want to do someting fun below, we are free to do so :)
+
+
+            int counter = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                counter += 1;
+                //Console.WriteLine($"Hanging around in Main()...waiting for someting fun... {counter}");
+            }
+        }
+    }
+}
+
+
+
